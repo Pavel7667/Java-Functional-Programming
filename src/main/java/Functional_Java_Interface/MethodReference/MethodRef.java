c621a5b3343a53fdd92bd83a862298a16fc3fa7f
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-12 23:03:04 +0200

    Bank Example

--- src/main/java/Functional_Java_Interface/MethodReference/MethodRef.java
+++ src/main/java/Functional_Java_Interface/MethodReference/MethodRef.java
@@ -1,16 +1,14 @@
 package Functional_Java_Interface.MethodReference;
 
-import java.util.function.IntConsumer;
-
 public class MethodRef {
     static int hireScope = 10;
 
     public static void main(String[] args) {
         // local scope
-        int anScope = 10;
-        IntConsumer intConsumer = (a) -> System.out.println(hireScope * 10);
-        IntConsumer ss = (a) -> System.out.println(anScope * 10);
-        anScope++;
+//        int anScope = 10;
+//        IntConsumer intConsumer = (a) -> System.out.println(hireScope * 10);
+//        IntConsumer ss = (a) -> System.out.println(anScope * 10);
+//        anScope++;
 
 
     }
