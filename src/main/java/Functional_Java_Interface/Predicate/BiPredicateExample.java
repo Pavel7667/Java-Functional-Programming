f5851b53d5f5ccaeaa9e297a94a8dab5dd916e03
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-12 17:32:32 +0200

    Predicate

---
+++ src/main/java/Functional_Java_Interface/Predicate/BiPredicateExample.java
@@ -0,0 +1,12 @@
+package Functional_Java_Interface.Predicate;
+
+import java.util.function.BiPredicate;
+
+public class BiPredicateExample {
+    public static void main(String[] args) {
+
+        BiPredicate<Boolean, Integer> p1 =
+                (online, years) -> online == true && years > 10;
+
+    }
+}
---
+++ src/main/java/Functional_Java_Interface/Predicate/Predicate_AND_BiConsumer.java
@@ -0,0 +1,26 @@
+package Functional_Java_Interface.Predicate;
+
+import Functional_Java_Interface.FunctionInstructors;
+import Functional_Java_Interface.Instructor;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+
+public class Predicate_AND_BiConsumer {
+
+    public static void main(String[] args) {
+        Predicate<Instructor> p1 = (i) -> i.isOnlineCourse();
+        Predicate<Instructor> p2 = (i) -> i.getYears() > 10;
+        BiConsumer<String, List<String>> biConsumer = (name, course) -> {
+            System.out.println(name + "   " + course);
+        };
+        List<Instructor> instructors = FunctionInstructors.getAll();
+        instructors.forEach(element -> {
+            if (p1.and(p2).test(element)) {
+                biConsumer.accept(element.getName(), element.getCourse());
+            }
+        });
+
+    }
+}
