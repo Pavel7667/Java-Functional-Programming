3044207e8de18205621c5411507720eab9b7c090
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-12 16:50:30 +0200

    BiConsumerExample

---
+++ src/main/java/Functional_Java_Interface/BiConsumerExample.java
@@ -0,0 +1,19 @@
+package Functional_Java_Interface;
+
+import java.util.function.BiConsumer;
+
+public class BiConsumerExample {
+    public static void main(String[] args) {
+
+        // print out SUM of Two numbers
+        BiConsumer<Integer, Integer> biConsumerIntSum =
+                (a, b) -> System.out.println(a + b);
+        biConsumerIntSum.accept(20, 20);
+
+        // print out String of Two numbers
+        BiConsumer<String, String> biConsumerStringConcat =
+                (a, b) -> System.out.println(a + b);
+        biConsumerStringConcat.accept("20", "20");
+
+    }
+}
