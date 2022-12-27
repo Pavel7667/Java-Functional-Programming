1813c5632ced0aaa2bb0dc6fc9f931854a9da007
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-27 10:12:31 +0200

    BiFunction

--- src/main/java/functionalprogrammingamigoscode/_Function.java
+++ src/main/java/functionalprogrammingamigoscode/_Function.java
@@ -1,19 +1,19 @@
 package functionalprogrammingamigoscode;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
-        Function<Integer, Integer> combineFunction =
-                incrementFunction.andThen(multiplyFunction);
-
-        Integer apply = combineFunction.apply(3);
-
+        Integer apply = combineBiFunction.apply(9, 10);
         System.out.println(apply);
     }
 
     // Functional Style
     static Function<Integer, Integer> incrementFunction = e -> ++e;
     static Function<Integer, Integer> multiplyFunction = e -> e * e;
+
+    static BiFunction<Integer, Integer, Integer> combineBiFunction =
+            (e, v) -> ((e + 1) * v);
 
 }
