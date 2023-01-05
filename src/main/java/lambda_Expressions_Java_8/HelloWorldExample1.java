3819c50af347a5c68437740d7d1b585fa01de2a2
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-05 17:40:52 +0200

    Default and Custom-Lambda @Override for Functional Interface

---
+++ src/main/java/lambda_Expressions_Java_8/HelloWorldExample1.java
@@ -0,0 +1,30 @@
+package lambda_Expressions_Java_8;
+
+public class HelloWorldExample1 implements HelloWorldInterface {
+
+    /**
+     * We can use default override
+     */
+    @Override
+    public String sayHelloWorld() {
+        return "Default override";
+    }
+
+    public static void main(String[] args) {
+
+        HelloWorldExample1 test = new HelloWorldExample1();
+
+        // generate custom Override for this Link
+        HelloWorldInterface test2 = () -> {
+            return "Override using Lambda_ONE";
+        };
+        // generate custom Override for this Link
+        HelloWorldInterface test3 = () -> {
+            return "Override using Lambda_TWO";
+        };
+
+        System.out.println(test.sayHelloWorld());
+        System.out.println(test2.sayHelloWorld());
+        System.out.println(test3.sayHelloWorld());
+    }
+}
--- src/main/java/lambda_Expressions_Java_8/HelloWorldInterface.java
+++ src/main/java/lambda_Expressions_Java_8/HelloWorldInterface.java
@@ -1,6 +1,10 @@
 package lambda_Expressions_Java_8;
 
 
+/**
+ * @FunctionalInterface this mark says that this interface can have only one abstract
+ * method
+ */
 @FunctionalInterface
 public interface HelloWorldInterface {
 
